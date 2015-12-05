7d54f394b8c25fbafc54ec9cb7636ef7541c1e5f
AlexTaber <alex.taber0@gmail.com>
2015-12-05 14:49:24 -0500

    service model/migration

---
+++ db/migrate/20151205194712_create_services.rb
@@ -0,0 +1,10 @@
+class CreateServices < ActiveRecord::Migration
+  def change
+    create_table :services do |t|
+      t.string :name, null: false
+      t.text   :description, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
