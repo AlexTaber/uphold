6e5e7fb02b167e784c158802e46d2e5026e93b44
AlexTaber <alex.taber0@gmail.com>
2015-12-05 14:26:47 -0500

    add band model/migration

---
+++ db/migrate/20151205191921_create_bands.rb
@@ -0,0 +1,11 @@
+class CreateBands < ActiveRecord::Migration
+  def change
+    create_table :bands do |t|
+      t.string  :name, null: false
+      t.text    :bio
+      t.integer :service_id
+
+      t.timestamps, null: false
+    end
+  end
+end
