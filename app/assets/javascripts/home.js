1f894a3d0053806c6304cc9040ca8d1a3c31d15a
AlexTaber <alex.taber0@gmail.com>
2015-12-06 01:50:30 -0500

    add jquery to service elements

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
   fadeInHeader();
+  $(".service").on("mouseenter", serviceEnter);
+  $(".service").on("mouseleave", serviceLeave);
 });
 
 function fadeInHeader() {
   $(".home-header-container").hide();
   $(".home-header-container").fadeIn(2000);
 }
+
+function serviceEnter(e) {
+  var tar = $(this).children().first();
+  tar.animate({
+    'marginTop' : "-=20px"
+  }, 200);
+  $(this).children(".service-text").hide().fadeIn(200);
+}
+
+function serviceLeave(e) {
+  var tar = $(this).children().first();
+  tar.animate({
+    'marginTop' : "+=20px"
+  }, 200);
+  $(this).children(".service-text").show().fadeOut(200);
+};
