6273c87790a61d8595d0b2447afb5cee2d768ff9
AlexTaber <alex.taber0@gmail.com>
2015-12-05 21:48:14 -0500

    implement mobile dropdown

--- app/assets/javascripts/mobile-nav.js
+++ app/assets/javascripts/mobile-nav.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-  $("#mobile-nav-menu").click(toggleMobileNav)
+  $("#mobile-nav-menu").click(toggleMobileNav);
+  $(".mobile-dropdown").click(toggleDropdown).children().click(function(e) {
+    return false;
+  });;
 });
 
 function toggleMobileNav(e) {
   $(".mobile-nav-items").slideToggle(200);
+}
+
+function toggleDropdown(e) {
+  $(".mobile-nav-services").slideToggle(200);
 }
