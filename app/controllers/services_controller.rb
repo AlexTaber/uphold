0073a1241f19dfa564c67aab28cc36f23be633c5
AlexTaber <alex.taber0@gmail.com>
2015-12-05 22:02:39 -0500

    add routes/controller methods/views for navbar links

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,4 +1,22 @@
 class ServicesController < ApplicationController
   def home
   end
+
+  def media
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def live
+  end
+
+  def management
+  end
+
+  def agency
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root 'services#home'
+
+  get '/about' => 'services#about'
+  get '/contact' => 'services#contact'
+  get '/media' => 'services#media'
+  get '/live' => 'services#live'
+  get '/management' => 'services#management'
+  get '/agency' => 'services#agency'
+  get '/venue-center' => 'venues#home'
 end
