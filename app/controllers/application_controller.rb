8bd0b17d2358009ac1cc3b2ed506d3d89f164a5c
AlexTaber <alex.taber0@gmail.com>
2015-12-07 02:26:39 -0500

    implement events#create controller method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,4 +26,19 @@ class ApplicationController < ActionController::Base
   def is_admin?
     current_user && current_user.admin
   end
+
+  def parse_datetime_params params, label, utc_or_local = :local
+    begin
+      year   = params[(label.to_s + '(1i)').to_sym].to_i
+      month  = params[(label.to_s + '(2i)').to_sym].to_i
+      mday   = params[(label.to_s + '(3i)').to_sym].to_i
+      hour   = (params[(label.to_s + '(4i)').to_sym] || 0).to_i
+      minute = (params[(label.to_s + '(5i)').to_sym] || 0).to_i
+      second = (params[(label.to_s + '(6i)').to_sym] || 0).to_i
+
+      return DateTime.civil_from_format(utc_or_local,year,month,mday,hour,minute,second)
+    rescue => e
+      return nil
+    end
+  end
 end
