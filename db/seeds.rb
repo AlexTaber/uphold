c898e8833977d99a67f68f92ea07ba6adce263d5
AlexTaber <alex.taber0@gmail.com>
2015-12-08 16:19:46 -0500

    initial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,22 @@ management = Service.create(
   description: "Band management"
 )
 
+15.times do |x|
+  band = Band.create(
+    name: Faker::Lorem.word.capitalize,
+    bio: Faker::Lorem.paragraph(3) + "\r\n\r\n" + Faker::Lorem.paragraph(2)
+  )
+end
+
+5.times do |x|
+  agency.bands << Band.find(x)
+  management.bands << Band.find(x + 5)
+end
+
+50.times do |x|
+  musician = Musician.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    bio: Faker::Lorem.paragraph(3) + "\r\n\r\n" + Faker::Lorem.paragraph(2)
+  )
+end
