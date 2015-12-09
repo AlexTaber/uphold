b1a7a92dffff01d62d8bad90d8c15ec6858603b7
AlexTaber <alex.taber0@gmail.com>
2015-12-08 22:56:17 -0500

    update html for live view

--- app/assets/javascripts/live.js
+++ app/assets/javascripts/live.js
@@ -1,31 +1,47 @@
 var scrollIndex = 0;
 
 $(document).ready(function() {
-  $(".carousel-left").click(scrollLeft);
-  $(".carousel-right").click(scrollRight);
+  $("#carousel-left").click(scrollLeft);
+  $("#carousel-right").click(scrollRight);
+  $("#carousel-left").hide();
 });
 
 function scrollLeft() {
-  subtractScrollIndex();
+  if(scrollIndex > 0) {
+    subtractScrollIndex();
 
-  $(".live-header-container").animate({
-    "marginLeft": "+=100vw"
-  }, 600);
+    $(".live-header-container").animate({
+      "marginLeft": "+=100vw"
+    }, 600);
 
-  postScroll();
+    postScroll();
+  }
 }
 
 function scrollRight() {
-  scrollIndex = (scrollIndex + 1) % 3;
+  if(scrollIndex < 2) {
+    scrollIndex = (scrollIndex + 1) % 3;
 
-  $(".live-header-container").animate({
-    "marginLeft": "-=100vw"
-  }, 600);
+    $(".live-header-container").animate({
+      "marginLeft": "-=100vw"
+    }, 600);
 
-  postScroll();
+    postScroll();
+  }
 }
 
 function subtractScrollIndex() {
   scrollIndex -= 1;
   if(scrollIndex < 0) scrollIndex = 2;
+}
+
+function postScroll() {
+  if(scrollIndex == 0) {
+    $("#carousel-left").fadeOut(200);
+  } else if(scrollIndex == 2) {
+    $("#carousel-right").fadeOut(200);
+  } else {
+    $("#carousel-left").fadeIn(200);
+    $("#carousel-right").fadeIn(200);
+  }
 }
--- app/models/event.rb
+++ app/models/event.rb
@@ -17,6 +17,14 @@ class Event < ActiveRecord::Base
     bookings.order(:rank)
   end
 
+  def headliners
+    bookings_by_rank[0...total_headliners]
+  end
+
+  def openers
+    bookings_by_rank[total_headliners...bookings.count]
+  end
+
   def reset_bookings_order
     bookings_by_rank.each_with_index { |booking, index| booking.update_attribute(:rank, index) }
   end
@@ -27,5 +35,21 @@ class Event < ActiveRecord::Base
 
   def active
     live && start_time > DateTime.now
+  end
+
+  def start_date_to_s
+     start_time.strftime('%A, %B %d')
+  end
+
+  def times_to_s
+    "#{start_time_to_s} - #{end_time_to_s}"
+  end
+
+  def start_time_to_s
+    start_time.strftime('%l:%M%p')
+  end
+
+  def end_time_to_s
+    end_time.strftime('%l:%M%p')
   end
 end
