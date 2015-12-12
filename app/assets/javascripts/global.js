56619d7671cde4f959e49fb0c880b5b1d6f88d27
AlexTaber <alex.taber0@gmail.com>
2015-12-12 06:30:48 -0500

    optimize relativeFade function

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,22 +1,54 @@
+var lastScroll = 0;
+var relativeFadeArr = [];
+var isUpdated = true;
+var windowHeight = $(window).height();
+var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+if ((is_chrome)&&(is_safari)) {is_safari=false;}
+
 $(document).ready(function() {
   var targets = $(".relative-fade");
-  if(targets.length > 0) {
+  if(targets.length > 0 && !is_safari) {
+    setUpRelativeFadeArr(targets);
     setInterval(function() { relativeFade(targets) }, 30);
   }
 });
 
 function relativeFade(targets) {
+  var curScroll = $(window).scrollTop();
+  if(curScroll != lastScroll) {
+    for(var i = 0; i < relativeFadeArr.length; i++) {
+      var tarHeight = relativeFadeArr[i].tar_height;
+      var targetMiddle = relativeFadeArr[i].middle;
+      var screenMiddle = curScroll + (windowHeight * 0.5);
+      var tarSize = (tarHeight * 0.5) + ((windowHeight * 0.5) * 0.8)
+      var amt = tarSize - Math.abs(screenMiddle - targetMiddle);
+      var opacity = Math.min(Math.max(0, amt/tarSize), 1);
+      if(opacity > 0) {
+        relativeFadeArr[i].selector.css({
+          "opacity": opacity * 1.25
+        });
+      }
+    }
+    lastScroll = curScroll;
+    isUpdated = false;
+  } else if(!isUpdated) {
+    updateRelativeFadeVars();
+    isUpdated = true;
+  }
+}
+
+function updateRelativeFadeVars() {
+  windowHeight = $(window).height();
+}
+
+function setUpRelativeFadeArr(targets) {
   for(var i = 0; i < targets.length; i++) {
     var tar = $(targets[i]);
-    var windowHeight = $(window).height();
-    var tarHeight = tar.height();
-    var screenMiddle = $(window).scrollTop() + (windowHeight * 0.5);
-    var targetMiddle = tar.position().top + (tarHeight * 0.5);
-    var tarSize = (tarHeight * 0.5) + ((windowHeight * 0.5) * 0.8)
-    var amt = tarSize - Math.abs(screenMiddle - targetMiddle);
-    var opacity = Math.min(Math.max(0, amt/tarSize), 1)
-    tar.css({
-      "opacity": opacity * 1.25
-    });
+    relativeFadeArr[i] = {
+      selector: tar,
+      tar_height: tar.height(),
+      middle: tar.position().top + (tar.height() * 0.5)
+    }
   }
 }
